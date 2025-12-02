cc92e36d8f1342bf42ba66cf4dbae26bfb749c19
belembelembelem <belemrs567@gmail.com>
2025-12-02 17:20:53 +0000

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,43 @@
+
+alert("Vamos começar o jogo!!!!!")
+alert("Vê quantas adivinhas consegues responder corretamente")
+alert("O que é o que é: passa diante do sol e não faz sombra?");
+let frase_1 = prompt("Resposta:");
+if (frase_1 === "vento") {
+    alert("Certíssimo, és bom nisto!");
+} else {
+    alert("Errado! A resposta era vento");
+}
+
+
+alert("O que é o que é: é cheia de furinhos, mas consegue reter água?");
+let frase_2 = prompt("Resposta:");
+if (frase_2 === "esponja") {
+    alert("Boa! Acertaste!");
+} else {
+    alert("Errado! A resposta era esponja");
+}
+
+alert("O que é o que é: quanto mais eu tiro, mais eu tenho?");
+let frase_3 = prompt("Resposta:");
+if (frase_3 === "buraco") {
+    alert("Boa! Certo!");
+} else {
+    alert("Errado! A resposta era buraco");
+}
+
+alert("Qual é o queijo que mais sente dor?");
+let frase_4 = prompt("Resposta:");
+if (frase_4 === "queijo ralado") {
+    alert("Acertaste!");
+} else {
+    alert("Errado! A resposta era queijo ralado");
+}
+
+alert("O que é o que é: tem cidades, lojas, ruas e nenhuma pessoa?");
+let frase_5 = prompt("Resposta:");
+if (frase_5 === "mapa") {
+    alert("Muito bem! Acertaste!");
+} else {
+    alert("Errado! A resposta era mapa");
+}
